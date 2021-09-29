b3b9c96705cee5e4c22463ce36bfa1d9ac2fd29b
JaroslavOndrus <jaroslavondrus04@gmail.com>
2021-09-29 20:37:31 +0200

    function that multiplies array by its lenght

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,4 @@
-const numberOfSlices = 24;
-const numberOfRecipients = 12;
-const slicesForEachOne = 2;
-
-console.log(EqualSlices(numberOfSlices, numberOfRecipients, slicesForEachOne))
-
-function EqualSlices(numberOfSlices, numberOfRecipients, slicesForEachOne){
-    
-    if((numberOfRecipients*slicesForEachOne)>numberOfSlices){
-        return false;
-    }
-    else{
-        return true;
-    }
+var nums = [1,0,3,3,7,2,1];
+for(let i = 0; i < nums.length; i++){
+    console.log(nums[i]*nums.length)
 }
