bde0a07ccfe6834061fc04e058b52dd32ad43d16
Jaroslav Ondruš <jaroslavondrus04@gmail.com>
2021-09-30 08:07:38 +0200

    function that checks if a string is a palindrome

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,13 @@
-/*const heightOfStep = 0.3;
-const lenghtOfStep = 0.2;
-const heightOfTower = 25;
+var str1 = "mom";
 
-console.log(totalDistance(heightOfStep, lenghtOfStep, heightOfTower))
+console.log(IsAPalindrome(str1));
 
-
-function totalDistance(heightOfStep, lenghtOfStep, heightOfTower){
-            return(heightOfTower/heightOfStep)*(heightOfStep+lenghtOfStep);
-}*/
-
-//
-
-/*const number1 = 3;
-const number2 = 4;
-const number3 = 3;
-
-console.log(EqualityOfThreeValues(number1, number2, number3))
-
-function EqualityOfThreeValues(number1, number2, number3){
-            if(number1 == number2 && number2 == number3){
-                        return 3;
-            }
-            else if(number1 == number2 || number1 == number3 || number2 == number3){
-                        return 2;
-            }
-            else{
-                        return 0;
-            }
-}*/
-
-//
-
-const tr1 = 3;
-const tr2 = 4;
-const tr3 = 5;
-
-console.log(IsItATriangle(tr1, tr2, tr3));
-
-function IsItATriangle(tr1, tr2, tr3){
-            if((tr1*tr1)+(tr2*tr2)==(tr3*tr3)){
+function IsAPalindrome(str1){
+            var palindrome = str1.split("").reverse().join("");
+            if(palindrome === str1){
                         return true;
             }
             else{
                         return false;
             }
 }
-
-//
-
-
-
-
