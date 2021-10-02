c630f07cc48a0545f442345c004549c656a49483
JaroslavOndrus <jaroslavondrus04@gmail.com>
2021-10-02 13:25:45 +0200

    function returns the larger - smaller of int array

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,24 @@
-var nums = [1,0,3,3,7,2,1];
-for(let i = 0; i < nums.length; i++){
-    console.log(nums[i]*nums.length)
+var array = [5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243];
+var even = 0;
+var odd = 0;
+
+for(let i = 0; i < array.length; i++){
+
+    if(array[i] %2 == 0){
+        even += array[i];
+    }
+    else{
+        odd += array[i];
+    }
+}
+
+if(even > odd){
+    console.log("Even > Odd");
+    var a = even - odd;
+    console.log(a);
+}
+else{
+    console.log("Odd > Even");
+    var b = odd - even;
+    console.log(b);
 }
